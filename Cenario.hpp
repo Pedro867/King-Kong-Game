acd7c1618380d06c3f17580977f61e07b99e5ebc
LucasLenzC <136482656+LucasLenzC@users.noreply.github.com>
2024-08-29 21:46:53 -0300

    Implementando o vetor de escadas
    A tentativa de implementação do vetor de escadas(incompleta)

--- Cenario.hpp
+++ Cenario.hpp
@@ -28,8 +28,7 @@ public:
 	Cenario(Player &player, sf::RenderWindow *window);
 
 	void desenhaCenario(sf::RenderWindow *window, Bomba &bomba);
-	void desenhaEscada(sf::RenderWindow *window, float larguraColuna,
-			float alturaLinha, int j, int i);
+	void desenhaEscada(sf::RenderWindow *window, float larguraColuna,float alturaLinha, int j, int i,vector<sf::FloatRect> teste);
 	void desenhaChao(sf::RenderWindow *window);
 	void desenhaParede(sf::RenderWindow *window);
 
@@ -71,11 +70,13 @@ Cenario::Cenario(Player &player, sf::RenderWindow *window) :
 }
 
 void Cenario::desenhaCenario(sf::RenderWindow *window, Bomba &bomba) {
-
+	vector<sf::FloatRect> teste;
 	bool playerBateuNoChao = false;
 	int playerBateuNaParede = 0;
 	bool bombaBateuNoChao = false;
 	int bombaBateuNaParede = 0;
+	int BombaBateuNaEscada = 0;
+	int PlayerBateuNaEscada = 0;
 
 	for (int i = 0; i < 10; i++) {
 		for (int j = 0; j < 40; j++) {
@@ -83,13 +84,13 @@ void Cenario::desenhaCenario(sf::RenderWindow *window, Bomba &bomba) {
 				chao[i].drawChao(window);
 			} else if (i == 1) {
 				if (j == 20 || j == 21) {
-					desenhaEscada(window, larguraColuna, alturaLinha, j, i);
+					desenhaEscada(window, larguraColuna, alturaLinha, j, i,teste);
 				}
 				chao[i].drawChao(window);
 				paredes[i].draw(window);
 			} else if (i == 2) {
 				if (j == 6 || j == 7 || j == 34 || j == 35) {
-					desenhaEscada(window, larguraColuna, alturaLinha, j, i);
+					desenhaEscada(window, larguraColuna, alturaLinha, j, i,teste);
 				}
 
 				chao[i].drawChao(window);
@@ -98,12 +99,12 @@ void Cenario::desenhaCenario(sf::RenderWindow *window, Bomba &bomba) {
 			} else if (i > 2 && i < 9) {
 				if (i % 2 == 0) {
 					if (j == 4 || j == 5 || j == 36 || j == 37) {
-						desenhaEscada(window, larguraColuna, alturaLinha, j, i);
+						desenhaEscada(window, larguraColuna, alturaLinha, j, i,teste);
 					}
 				}
 				if (i % 2 == 1) {
 					if (j == 20 || j == 21) {
-						desenhaEscada(window, larguraColuna, alturaLinha, j, i);
+						desenhaEscada(window, larguraColuna, alturaLinha, j, i,teste);
 					}
 				}
 				chao[i].drawChao(window);
@@ -115,8 +116,7 @@ void Cenario::desenhaCenario(sf::RenderWindow *window, Bomba &bomba) {
 			}
 
 			playerBateuNoChao += playerTestaColisao(&playerBateuNaParede, i);
-			bombaBateuNoChao += bombaTestaColisao(bomba, &bombaBateuNaParede,
-					i);
+			bombaBateuNoChao += bombaTestaColisao(bomba, &bombaBateuNaParede,i);
 		} //for j
 	} //for i
 	  //logica: se ele bateu em algum chao, bateuNoChao>0
@@ -144,9 +144,8 @@ void Cenario::desenhaCenario(sf::RenderWindow *window, Bomba &bomba) {
 	}
 } //fim func
 
-void Cenario::desenhaEscada(sf::RenderWindow *window, float larguraColuna,
-		float alturaLinha, int j, int i) { //int j para desenhar exatamente na chunk
-
+void Cenario::desenhaEscada(sf::RenderWindow *window, float larguraColuna,float alturaLinha, int j, int i,vector<sf::FloatRect> teste) { //int j para desenhar exatamente na chunk
+	sf::FloatRect hitbox;
 	sf::Texture texturaEscada;
 	sf::Sprite escada;
 	texturaEscada.loadFromFile("assets/Escada.png");
@@ -154,6 +153,11 @@ void Cenario::desenhaEscada(sf::RenderWindow *window, float larguraColuna,
 	escada.setOrigin(8, 0);
 	escada.setScale(larguraColuna / 8, alturaLinha / 8);
 	escada.setPosition(j * larguraColuna, alturaLinha * i);
+
+
+	hitbox = escada.getGlobalBounds();
+	teste.push_back(hitbox);
+
 
 	window->draw(escada);
 }
@@ -213,25 +217,25 @@ bool Cenario::bombaTestaColisao(Bomba &bomba, int *bombaBateuNaParede, int i) {
 
 /*bool Cenario::iniciarKong(sf::RenderWindow *window) {
 
-	int velX = larguraColuna / 10.0;
-	int velY = -alturaLinha / 25.0;
+ int velX = larguraColuna / 10.0;
+ int velY = -alturaLinha / 25.0;
 
-	if (kong.getPosition().x <= larguraColuna * 7) {
-		velX = -velX;
-	} else if (kong.getPosition().x >= larguraColuna * 9) {
-		//Bug ta nesse else if
-		velX = -velX;
-		int y = kong.getPosition().y;
-		int x = kong.getPosition().x;
-		kong.setPosition(x - velX, y);
-	}
+ if (kong.getPosition().x <= larguraColuna * 7) {
+ velX = -velX;
+ } else if (kong.getPosition().x >= larguraColuna * 9) {
+ //Bug ta nesse else if
+ velX = -velX;
+ int y = kong.getPosition().y;
+ int x = kong.getPosition().x;
+ kong.setPosition(x - velX, y);
+ }
 
-	kong.move(velX, velY);
+ kong.move(velX, velY);
 
-	window->draw(kong);
+ window->draw(kong);
 
-	return true;
-}*/
+ return true;
+ }*/
 
 void Cenario::setaAndarBomba(Bomba &bomba) {
 	float altura, largura;
